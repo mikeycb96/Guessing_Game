720c32fdf313bd7aab798718f1e2f1677a8dc968
mikeycb96 <michaelsonbaptist@gmail.com>
2023-07-06 16:05:47 +0100

    add WeatherCHeck.java

---
+++ src/WeatherCheck.java
@@ -0,0 +1,27 @@
+public class WeatherCheck {
+
+    public static void main(String[] args) {
+
+        int temperature = 20;
+        boolean currentlyRaining = true;
+
+        if(temperature < 10 && currentlyRaining){
+            System.out.println("Wear a waterproof coat");
+        } else if(temperature < 10 && currentlyRaining == false){
+            System.out.println("Wear a coat");
+        } else if(temperature >= 11 && temperature <= 20 && currentlyRaining){
+            System.out.println("Light raincoat needed");
+        } else if(temperature >= 11 && temperature <= 20 && currentlyRaining == false){
+            System.out.println("Fine weather");
+        } else if(temperature > 21 && currentlyRaining) {
+            System.out.println("Hot but need an umbrella");
+        } else if(temperature > 21 && currentlyRaining == false){
+            System.out.println("Hot so need shorts!");
+        }
+
+
+
+
+
+    }
+}
